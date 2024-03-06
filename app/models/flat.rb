7dbb1c9b1ef94abf3e49d7e50a1f1c75fdeb572d
Christian Bongard <christian.bongard90@gmail.com>
2024-03-06 09:40:01 +0400

    Added lat and long to flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  geocoded_by :address # geocoding the address of the flat
+  after_validation :geocode, if: :will_save_change_to_address? # after validation, geocode the address if it has changed
 end
