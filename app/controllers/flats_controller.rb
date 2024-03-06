db8305d42d54224e194420fd3a50a251b51e05b9
Christian Bongard <christian.bongard90@gmail.com>
2024-03-06 10:18:19 +0400

    added custom marker

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,7 +9,8 @@ class FlatsController < ApplicationController
       {
         lat: flat.latitude,
         lng: flat.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: { flat: flat })
+        info_window_html: render_to_string(partial: "info_window", locals: { flat: flat }),
+        marker_html: render_to_string(partial: "marker")
       }
     end
   end
