ead7fb5df630a845e45d9e254174f906bc1383dc
Christian Bongard <christian.bongard90@gmail.com>
2024-03-06 10:07:59 +0400

    added                 fiting to markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,11 +16,21 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10",
     });
     this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
     });
+  }
+
+  #fitMapToMarkers() {
+    // Create a boundary
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 8000 });
   }
 }
