744407d9eccbc77af572953b1a0b0c714d09280f
Christian Bongard <christian.bongard90@gmail.com>
2024-03-06 10:21:39 +0400

    Added the geocoder for mapbox

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,7 +10,7 @@ class FlatsController < ApplicationController
         lat: flat.latitude,
         lng: flat.longitude,
         info_window_html: render_to_string(partial: "info_window", locals: { flat: flat }),
-        marker_html: render_to_string(partial: "marker")
+        marker_html: render_to_string(partial: "marker", locals: { flat: flat })
       }
     end
   end
